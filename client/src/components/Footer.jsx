092592acc5d50315ee560984cd717e87742816a4
somilyadav7 <mesomilyadav7@gmail.com>
2024-05-12 21:06:48 +0530

    Make chaanges int the footer, changed the text color for the email and improved the css styling of the footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
       </div>
 
       <div className='bg-[#1d4ed8] '>
-        <div className='container px-5 py-20 mx-auto '>
+        <div className='container px-5 py-10 mx-auto '>
           <div className='w-full flex flex-wrap gap-10 justify-between -mb-10 -px-4'>
             {footerLinks.map(({ id, title, links }) => (
               <div className='w-auto px-4 ' key={id + title}>
@@ -33,7 +33,7 @@ const Footer = () => {
                   {title}
                 </h2>
 
-                <div className='mb-10 flex flex-col gap-3 '>
+                <div className=' flex flex-col gap-2 '>
                   {links.map((link, index) => (
                     <Link
                       key={link + index}
@@ -50,43 +50,38 @@ const Footer = () => {
         </div>
 
         <div className=''>
-          <p className='container px-5 mx-auto text-white mt-2 '>
-            Subscribe to our Newsletter
-          </p>
-
-          <div className='container mx-auto px-5 pt-6 pb-8 flex flex-wrap items-center justify-between '>
-            <div className='w-full md:w-2/4 lg:w-1/3 h-16 flex items-center justify-center md:justify-start '>
-              <TextInput
-                styles='w-full flex-grow md:w-40 2xl:w-64 bg-gray-100 sm:mr-4 md-2'
-                type='email'
-                placeholder='Email Address'
-              />
-
-              <CustomButton
-                title='Subscribe'
-                containerStyles={
-                  "block bg-[#001a36] text-white px-5 py-2.5 text-md rounded hover:bg-blue-800 focus:potline-none flex-col items-center mt-2"
-                }
-              />
-            </div>
-
-            <span className='inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto'>
-              <a className='text-white text-xl  hover:scale-125 ease-in-out duration-300'>
-                <FaFacebookF />
-              </a>
-              <a className='ml-3 text-white text-xl  hover:scale-125 ease-in-out duration-300'>
-                <FaTwitter />
-              </a>
-              <a className='ml-3 text-white text-xl  hover:scale-125 ease-in-out duration-300'>
-                <FiInstagram />
-              </a>
-
-              <a className='ml-3 text-white text-xl  hover:scale-125 ease-in-out duration-300'>
-                <FaLinkedinIn />
-              </a>
-            </span>
+          <div className="flex flex-col items-center mb-4">
+          <p className="text-sm mb-2">Subscribe to our Newsletter</p>
+          <div className="flex items-center mb-4 text-gray-500">
+            <TextInput
+              type="email"
+              placeholder="Email Address"
+              containerStyles="bg-gray-100 mr-2 w-48 sm:w-64 "
+            />
+            <CustomButton
+              title="Subscribe"
+              containerStyles="bg-blue-900 text-white px-4 py-2 ml-4 mt-3 rounded hover:bg-blue-800 focus:outline-none"
+            />
           </div>
         </div>
+
+        {/* Social Icons */}
+        <div className="flex items-center justify-center space-x-4 mb-8">
+          <a href="#" className="text-2xl hover:text-gray-300">
+            <FaFacebookF />
+          </a>
+          <a href="#" className="text-2xl hover:text-gray-300">
+            <FaTwitter />
+          </a>
+          <a href="#" className="text-2xl hover:text-gray-300">
+            <FiInstagram />
+          </a>
+          <a href="#" className="text-2xl hover:text-gray-300">
+            <FaLinkedinIn />
+          </a>
+        </div>
+      </div>
+
 
         <div className='bg-[#001a36]'>
           <div className='container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row'>
