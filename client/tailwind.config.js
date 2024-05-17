d0cb59b533179ecd6f43f4424572aba31f89b6a6
yashmandi <yashmandi18@gmail.com>
2024-05-18 01:05:50 +0530

    #65 Loader Implmentation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        bounce: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-15px)" },
+        },
+      },
+      animation: {
+        bounce1: "bounce 0.6s infinite",
+        bounce2: "bounce 0.6s infinite 0.2s",
+        bounce3: "bounce 0.6s infinite 0.4s",
+      },
+    },
   },
   plugins: [],
 };
