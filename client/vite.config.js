4fbea1523c4ed1c47aab818c40eda03f550415c0
Ananya <shavijain2125@gmail.com>
2024-06-15 11:23:01 +0530

    rollup options

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,10 +7,19 @@ export default defineConfig({
     // Add other plugins as needed
   ],
   build: {
+    chunkSizeWarningLimit: 1000, // Example: increase limit to 1000 kB (1 MB)
     rollupOptions: {
       external: [
-      'slick-carousel/slick/slick.css'
-      ]
+        'slick-carousel/slick/slick.css'
+      ],
+      output: {
+        manualChunks(id) {
+          // Example: group large dependencies or chunks manually
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
       }
+    }
   }
 });
