9c54ec4dc63ce490161d48b549095e2dca08cb99
MadalapureVaishnavi <madalapurevaishnavi@gmail.com>
2024-06-24 20:51:46 +0530

    added image in title

--- server/dbConfig/dbConnection.js
+++ server/dbConfig/dbConnection.js
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
 
 const dbConnection = async () => {
     try {
-        const dbConnection = await mongoose.connect(process.env.MONGODB_URL);
+        const dbUri = process.env.MONGO_URL;
+        if (!dbUri) {
+            throw new Error("MONGO_URL environment variable is not set.");
+        }
+
+        await mongoose.connect(dbUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
         console.log("Database connected successfully");
-
-
     } catch (error) {
-        console.log('MongoConnection Failed !!' + error );
+        console.log('MongoConnection Failed !! ' + error);
     }
 }
 
